17c571a416664644c72f6533003a80e30c5cd141
oybek703 <hhoybek@gmail.com>
2020-12-19 16:08:05 +0500

    mobile responsivenes is added to services block

--- src/components/UI/Service.js
+++ src/components/UI/Service.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react'
-import {makeStyles, Typography} from "@material-ui/core"
+import {makeStyles, Typography, useMediaQuery} from "@material-ui/core"
 import Grid from "@material-ui/core/Grid"
 import LearnButton from "./LearnButton"
 
@@ -15,13 +15,16 @@ const useStyles = makeStyles(theme => ({
 
 const Service = ({title, text, img, subtext, link}) => {
     const classes = useStyles()
+    const matchXS = useMediaQuery(theme => theme.breakpoints.down('xs'))
     return (
         <Fragment>
             <Grid item>
-                <Typography variant='h4' className={classes.title}>{title}</Typography>
-                <Typography gutterBottom variant='subtitle1' className={classes.text}>{text}</Typography>
-                <Typography gutterBottom variant='subtitle2' className={classes.text}>{subtext}</Typography>
-                <LearnButton link={link}/>
+                <Typography align={matchXS ? 'center' : 'left'} variant='h4' className={classes.title}>{title}</Typography>
+                <Typography align={matchXS ? 'center' : 'left'} gutterBottom variant='subtitle1' className={classes.text}>{text}</Typography>
+                <Typography align={matchXS ? 'center' : 'left'} gutterBottom variant='subtitle2' className={classes.text}>{subtext}</Typography>
+                <Grid item container justify={matchXS ? 'center' : 'flex-start'}>
+                    <LearnButton link={link}/>
+                </Grid>
             </Grid>
             <Grid item>
                 <img src={img} alt={`${title} logo`}/>
